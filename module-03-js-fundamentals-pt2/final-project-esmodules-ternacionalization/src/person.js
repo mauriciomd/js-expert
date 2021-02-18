fe246b7cf2900ce782d84b8c3fd32f321dfcabab
Mauricio Matter Donato <mauricio.fear@gmail.com>
2021-02-18 19:29:49 -0300

    feat: [module-03] add final project part 1

---
+++ module-03-js-fundamentals-pt2/final-project-esmodules-ternacionalization/src/person.js
@@ -0,0 +1,32 @@
+export default class Person {
+  constructor({ id, vehicles, kmTraveled, from, to }) {
+    this.id = id
+    this.vehicles = vehicles
+    this.kmTraveled = kmTraveled
+    this.from = from
+    this.to = to
+  }
+
+  formatted(lang) { 
+    const mapDate = date => {
+      const [year, month, day] = date.split('-').map(Number)
+      return new Date(year, month - 1 , day)
+    }
+
+    return {
+      id: Number(this.id),
+      vehicles: new Intl
+                    .ListFormat(lang, { style: 'long', type: 'conjunction' })
+                    .format(this.vehicles),
+      kmTraveled: new Intl
+                    .NumberFormat(lang, { style: 'unit', unit: 'kilometer' })
+                    .format(this.kmTraveled),
+      from: new Intl
+                    .DateTimeFormat(lang, { month: 'long', day: '2-digit', year: 'numeric' })
+                    .format(mapDate(this.from)),
+      to: new Intl
+                    .DateTimeFormat(lang, { month: 'long', day: '2-digit', year: 'numeric' })
+                    .format(mapDate(this.to)),
+    }
+  }
+}
