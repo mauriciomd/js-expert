fa381ca047eeb04773bb5f8b5da1401a33a7153c
Mauricio Matter Donato <mauricio.fear@gmail.com>
2021-03-17 17:03:27 -0300

    feat: [module-05] add the code developed in the class #01

---
+++ module-05-regex/class-01-vscode-terminal-regex/regex-terminal.sh
@@ -0,0 +1,25 @@
+# root dir
+find . -name *.test.js
+find . -name *.test.js -not -path '*node_modules**'
+find . -name *.js -not -path '*node_modules**'
+
+npm i -g ipt
+find . -name *.js -not -path '*node_modules**' | ipt
+
+# go back to 
+cp -r ../../module-01-js-testing/final-project-bdd-tdd .
+
+CONTENT="'use strict';"
+find . -name *.js -not -path '*node_modules**' \
+| ipt -o \
+| xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
+/g' {file}
+
+# 1s -> first line
+# ^-> first column
+# replace by $CONTENT
+# Add an implicit \n
+
+find . -name *.js -not -path '*node_modules**' \
+| xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
+/g' {file}
